2f47db74693f4e5090b49c3c08f4dbea158a5bbb
Alex Arriaga <alex.arriaga@webtraining.world>
2021-09-23 14:57:49 -0700

    Checkpoint 1. Componente no accessible

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import Schedule from "./components/Schedule/Schedule";
+import talks from "./data/talks.json";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App container mt-4">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Hello world!
+          <Schedule talks={talks}/>
       </header>
+
     </div>
   );
 }
---
+++ src/components/Schedule/Schedule.js
@@ -0,0 +1,26 @@
+function Schedule({talks}) {
+    return (
+        <ul className="talks">
+            {talks.map(talk => (
+
+                <li className="talk">
+                    <div className="row">
+                        <div className="col-2">
+                            <img src={talk.image} className="img-fluid"/>
+                        </div>
+                        <div className="col-10">
+                            <span className="talk-date">
+                                {talk.date}
+                            </span>
+                            <a href="#"><h2 className="mt-3 talk-title">{talk.title}</h2></a>
+                            <p className="mb-0">{talk.description}</p>
+                        </div>
+                    </div>
+                </li>
+
+            ))}
+        </ul>
+    );
+}
+
+export default Schedule;
